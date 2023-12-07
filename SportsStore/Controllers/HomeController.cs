357fd20c58e70af7ea969c985c0bdef31ffd98e4
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-12-07 16:06:52 +0300

    194

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -15,10 +15,11 @@ namespace SportsStore.Controllers
             _repository = repository;
         }
 
-        public ViewResult Index(int productPage = 1) => 
+        public ViewResult Index(string category, int productPage = 1) => 
             View(new ProductsListViewModel
             {
                 Products = _repository.Products
+                    .Where(p => category == null || p.Category == category)
                     .OrderBy(p => p.ProductID)
                     .Skip((productPage - 1) * PageSize)
                     .Take(PageSize),
@@ -26,8 +27,11 @@ namespace SportsStore.Controllers
                 {
                     CurrentPage = productPage,
                     ItemsPerPage = PageSize,
-                    TotalItems = _repository.Products.Count()
-                }
+                    TotalItems = category == null 
+                        ? _repository.Products.Count()
+                        : _repository.Products.Where(e => e.Category == category).Count()
+                },
+                CurrentCategory = category
             });
     }
 }
